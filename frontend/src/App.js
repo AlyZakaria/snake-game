b58536d7218756ce65a8924cfd5e0c452e307e5a
AlyZakaria <77212759+AlyZakaria@users.noreply.github.com>
2023-07-09 15:52:02 +0300

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/Login/Login';
 
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/login" component={Login} />
+        {/* <Route path="/register" component={Register} /> */}
+        <Route path="/" component={Login} />
+      </Routes>
     </div>
   );
 }
