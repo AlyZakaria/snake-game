68254d72511c1626125172ec59de3a532703b1ca
AmrElkhatieb <amr1kemon1@gmail.com>
2023-07-09 11:40:36 +0300

    signout and some bug fixes

--- backend/services/AuthService.js
+++ backend/services/AuthService.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const {Users} = require('../models')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+expiration = 24 * 60 * 60
+const createToken = (id) => {
+    return jwt.sign({ id }, 'somesecret', {
+      expiresIn: expiration
+    });
+  };
 
 const signup = async (user) => {
     console.log(user);
     try{
         const salt = await bcrypt.genSalt();
-        console.log(salt)
         user.password = await bcrypt.hash(user.password, salt);
         const created = await Users.create(user);
-        console.log(created);
         return user.username;
     }
     catch(err){
@@ -21,28 +27,24 @@ const signup = async (user) => {
 
 const signin = async(username, password) => {
     try{
-        const user = Users.findOne({where: {username: username, password: password}})
+        const user = await Users.findOne({where: {username: username}})
         const success = await bcrypt.compare(password, user.password);
         if (success){
-            return user.username;
+            const token = createToken(user.user_id);
+            return token;
+        }
+        else{
+            return 'Wrong password';
         }
     }
     catch(err){
-        return err;
-    }
-    
-}
-
-
-const signout = async(username) => {
-    try{
-
-    }
-    catch(err){
-        console.log(err);
+        console.log(err)
+        return 'Error Finding User';
     }
 }
+
+
+
 
 module.exports.signup = signup
 module.exports.signin = signin
-module.exports.signout = signout
