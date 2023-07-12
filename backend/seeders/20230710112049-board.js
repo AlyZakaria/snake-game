f491f577ceafb64991cb2d116c8311264155f938
AmrElkhatieb <94713322+AmrElkhatieb@users.noreply.github.com>
2023-07-12 10:40:49 +0300

    Update 20230710112049-board.js

--- backend/seeders/20230710112049-board.js
+++ backend/seeders/20230710112049-board.js
@@ -10,23 +10,23 @@ module.exports = {
 
     await queryInterface.insert(null, 'boards', g);
     const boardElementsData = [
-      [1, 1, 38, 'l',   2],
-      [2, 4, 14, 'l',   2],
-      [3, 8, 10, 'l',   2],
-      [4, 21, 42, 'l',  2],
-      [5, 28, 76, 'l',  2],
-      [6, 36, 6, 's',   2],
-      [7, 32, 10, 's',  2],
-      [8, 48, 26, 's',  2],
-      [9, 50, 67, 'l',  2],
-      [10, 63, 18, 's', 2],
-      [11, 71, 92, 'l', 2],
-      [12, 80, 99, 'l', 2],
-      [13, 88, 24, 's', 2],
-      [14, 95, 56, 's', 2],
-      [15, 97, 78, 's', 2],
-      [16, 88, 24, 's', 2],
-      [17, 97, 78, 's', 2]
+      [1, 1, 38, 'l',   1],
+      [2, 4, 14, 'l',   1],
+      [3, 8, 10, 'l',   1],
+      [4, 21, 42, 'l',  1],
+      [5, 28, 76, 'l',  1],
+      [6, 36, 6, 's',   1],
+      [7, 32, 10, 's',  1],
+      [8, 48, 26, 's',  1],
+      [9, 50, 67, 'l',  1],
+      [10, 63, 18, 's', 1],
+      [11, 71, 92, 'l', 1],
+      [12, 80, 99, 'l', 1],
+      [13, 88, 24, 's', 1],
+      [14, 95, 56, 's', 1],
+      [15, 97, 78, 's', 1],
+      [16, 88, 24, 's', 1],
+      [17, 97, 78, 's', 1]
     ];
     const boardElementsColumns = ['board_element_id', 'from', 'to', 'type', 'board_id'];
     await queryInterface.bulkInsert('board_elements', boardElementsData.map(data => Object.fromEntries(boardElementsColumns.map((col, i) => [col, data[i]]))));
