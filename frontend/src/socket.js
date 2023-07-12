46c999cfdfb474c1fe51ac1b33ac2f81eaf17daa
AmrElkhatieb <amr1kemon1@gmail.com>
2023-07-12 14:57:02 +0300

    integrating

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,5 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = 'http://localhost:3000';
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ['websocket']
+});
 
